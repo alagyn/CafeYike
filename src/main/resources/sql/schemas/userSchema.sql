cb430d6278f5f48ff0bdf4299912af41ad136a29
alagyn <benkimbrough44@gmail.com>
2022-10-24 13:28:53 -0400

    Update user upserts

--- src/main/resources/sql/schemas/userSchema.sql
+++ src/main/resources/sql/schemas/userSchema.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS users (
     guild_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     yike_count INTEGER NOT NULL DEFAULT 0,
-    user_quote_id INTEGER NOT NULL UNIQUE,
+    user_quote_id INTEGER NOT NULL UNIQUE, 
     PRIMARY KEY(guild_id, user_id)
 )
--- src/main/resources/sql/yikes/incYike.sql
+++ src/main/resources/sql/yikes/incYike.sql
@@ -1,6 +1,6 @@
 INSERT INTO users
 (guild_id, user_id, yike_count, user_quote_id)
-VALUES ( ? , ? , 1, last_insert_rowid() )
+VALUES ( ? , ? , 1, (SELECT count(*) FROM users) )
 ON CONFLICT DO
 UPDATE SET yike_count = yike_count + 1
 RETURNING yike_count
