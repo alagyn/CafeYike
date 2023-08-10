72c661f5efc9f33256acb3d6ad091377d4672df5
alagyn <benkimbrough44@gmail.com>
2023-08-10 17:39:02 -0400

    Add start-detached.sh

---
+++ start-detached.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+home=$(realpath $(dirname $0))
+
+DB_DIR=$home/dat/
+DB_DIR=`realpath $DB_DIR`
+USER=root
+
+docker run \
+    --rm \
+    -ti \
+    --user $USER \
+    --hostname cafe-yike \
+    --workdir /home/$USER \
+    -p 8000:8000 \
+    -e YM_SYS_CFG=/home/$USER/system.conf \
+    -e CafeYikeDB=/home/$USER/dat/cafe.db \
+    -v $DB_DIR:/home/$USER/dat/ \
+    cafe-yike:1 "/home/$USER/start_manager.sh" > manager.log 2>&1 &
